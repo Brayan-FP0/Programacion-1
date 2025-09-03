340d18d82cdd9f0f74687ea3a4e504fcbcdda304
Brayan-FP0 <brayanpinilla.com@gmail.com>
2025-09-03 14:55:49 -0500

    comparacion de operadores

---
+++ operadores_comparacion.py
@@ -0,0 +1,15 @@
+#operadores de comparacion
+num1 = 5
+num2 = 10
+#igualdad
+print (num1==num2)
+#diferencia
+print(num1!= num2)
+#menor que 
+print(num1<num2)
+#mayor que 
+print(num1>num2)
+#mayor o igual
+print(num1>=num2)
+#menor o igual
+print(num1<=num2)
